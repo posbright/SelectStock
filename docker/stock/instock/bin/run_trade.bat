f71527346ebc9adb703d0c5149d3833e5acde14d
Zheng ming Ding (CSI INTERFUSION INC) <v-zhending@microsoft.com>
2026-03-13 14:34:03 +0800

    chore: 暂时移除 docker 目录（回测开发分支）

--- docker/stock/instock/bin/run_trade.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001
-@echo off
-cd %~dp0
-cd ..
-cd trade
-python trade_service.py
-echo ------交易服务已启动，请不要关闭------
-pause
-exit
--- docker/stock/instock/lib/version.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = 'InStock'
-__date__ = '2026/02/14'
-
-__version__ = "4.0.0"
-# 每次发布时候更新。
